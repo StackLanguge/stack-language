44a68ab9e25daab67d4f2267c95e553a57659c54
liam4 <liamdraws4@gmail.com>
2015-10-30 17:13:40 -0300

    Probably should have made a `comments` branch but oh well for now

--- stack/stack_parser.py
+++ stack/stack_parser.py
@@ -35,30 +35,37 @@ def _parse_iter(prog):
     prog_len = len(prog) - 1
     prog_index = -1
     tokens = []
+    in_comment = False
     while prog_index < prog_len:
         prog_index += 1
         tok = prog[prog_index]
-        if is_op(tok):
-            tokens.append(Token(TYPE='op', VAL=tok))
-        elif is_str(tok):
-            tokens.append(Token(TYPE='str', VAL=tok[1:-1]))
-        elif is_num(tok):
-            tokens.append(Token(TYPE='num', VAL=float(tok)))
-        elif is_bool(tok):
-            tokens.append(Token(
-                TYPE='bool', VAL=(True if tok == 'True' else False)))
-        elif is_list(tok):
-            tokens.append(Token(TYPE='list', VAL=[]))
-        elif is_name(tok):
-            tokens.append(Token(TYPE='name', VAL=tok[1:]))
-        elif is_var(tok):
-            tokens.append(Token(TYPE='var', VAL=tok))
-        elif tok == BLOCK_START:
-            code, end = _parse_iter(prog[prog_index+1:])
-            prog_index += end + 1
-            tokens.append(Token(TYPE='code', VAL=code))
-        elif tok == BLOCK_END:
-            break
+        if in_comment:
+            if tok == COMMENT_END:
+                in_comment = False
+        else:
+            if tok == COMMENT_START:
+                in_comment = True
+            elif is_op(tok):
+                tokens.append(Token(TYPE='op', VAL=tok))
+            elif is_str(tok):
+                tokens.append(Token(TYPE='str', VAL=tok[1:-1]))
+            elif is_num(tok):
+                tokens.append(Token(TYPE='num', VAL=float(tok)))
+            elif is_bool(tok):
+                tokens.append(Token(
+                    TYPE='bool', VAL=(True if tok == 'True' else False)))
+            elif is_list(tok):
+                tokens.append(Token(TYPE='list', VAL=[]))
+            elif is_name(tok):
+                tokens.append(Token(TYPE='name', VAL=tok[1:]))
+            elif is_var(tok):
+                tokens.append(Token(TYPE='var', VAL=tok))
+            elif tok == BLOCK_START:
+                code, end = _parse_iter(prog[prog_index+1:])
+                prog_index += end + 1
+                tokens.append(Token(TYPE='code', VAL=code))
+            elif tok == BLOCK_END:
+                break
     return tokens, prog_index
 
 
@@ -74,3 +81,4 @@ def compile_to_file(prog, filename):
 
 if __name__ == '__main__':
     print(parse("5 `n set n"))
+    print(parse("'hello' /* this is a comment */ 'world'"))
