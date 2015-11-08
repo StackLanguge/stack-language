712bdf50eb010bb48f97fc85598f404d4a34541c
liam4 <liamdraws4@gmail.com>
2015-11-08 08:55:14 -0400

    Lib stuff
    Just seeing if I can import things from Python..
    
    Oh hey, it looks like I can! :D

--- lib/import_from_python.py
+++ lib/import_from_python.py
@@ -1,4 +1,6 @@
 import importlib
+import inspect
+import types
 
 
 def import_from_python(interpeter, stack, scopes, stream):
@@ -8,6 +10,44 @@ def import_from_python(interpeter, stack, scopes, stream):
     stack.append(tok)
 
 
+def py_call(interpeter, stack, scopes, stream):
+    val2, val = stack.pop(), stack.pop()
+
+    if not val.TYPE == "py-obj":
+        interpeter.report_error(
+            "TYPE",
+            "py_call",
+            "%s is not a py-obj!" % val)
+    if not isinstance(val.VAL, types.FunctionType):
+        interpeter.report_error(
+            "TYPE",
+            "py_call",
+            "%s.VAL is not a Python function!")
+
+    f = val.VAL
+
+    spec = inspect.getargspec(f)
+
+    defaults = zip(reversed(spec.args), reversed(spec.defaults))
+    kwargs = dict(defaults)
+
+    args = []
+    for arg in val2.VAL:
+        args.append(arg)
+
+    # print("KW-ARGS:", kwargs)
+    # print("ARGS:", args)
+    # print("FUNC:", f)
+
+    res = f(*args, **kwargs)
+
+    # print("RES:", repr(res))
+
+    tok = interpeter.Token(TYPE="py-obj", VAL=res)
+    stack.append(tok)
+
+
 module = {
-    "import_from_python": import_from_python
+    "import_from_python": import_from_python,
+    "py_call": py_call
 }
--- lib/oop_base.py
+++ lib/oop_base.py
@@ -1,3 +1,6 @@
+import types
+
+
 def mapobj(interpeter, stack, scopes, stream):
     val1 = stack.pop()
     obj = {}
@@ -16,19 +19,38 @@ def newobj(interpeter, stack, scopes, stream):
 
 def prop(interpeter, stack, scopes, stream):
     val2, val1 = stack.pop().VAL, stack.pop().VAL
-    if not val2 in val1.keys():
+
+    if isinstance(val2, dict):
+        if not val2 in val1.keys():
+            interpeter.report_error(
+                "OOP_ERROR",
+                "propof",
+                "Property not in object!")
+    elif "__all__" in dir(val2):
+        if not val2 in val1.__all__:
+            interpeter.report_error(
+                "OOP_ERROR",
+                "propof",
+                "Property not in object!")
+    elif not val2 in dir(val1):
         interpeter.report_error(
             "OOP_ERROR",
             "propof",
             "Property not in object!")
-    value = val1[val2]
-    tok = interpeter.Token(TYPE='str', VAL=value)
+
+    if isinstance(val1, types.ModuleType):
+        value = val1.__dict__[val2]
+    else:
+        value = val1[val2]
+
+    tok = interpeter.Token(TYPE='py-obj', VAL=value)
     stack.append(tok)
 
 
 def setprop(interpeter, stack, scopes, stream):
     val3, val2, val1 = [i.VAL for i in (stack.pop(), stack.pop(), stack.pop())]
     val1[val2] = val3
+    stack.append(val1)
 
 
 module = {
