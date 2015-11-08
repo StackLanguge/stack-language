720c74b10a6cbdd709ba0c2aeea95fad2489b89c
liam4 <liamdraws4@gmail.com>
2015-11-07 20:54:51 -0400

    File input
    No output though lelelelelel. :D

--- lib/fileio.py
+++ lib/fileio.py
@@ -1,6 +1,22 @@
-def blah(*args):
-    print("BLAHHHH")
+import os.path
+
+
+def fopen(interpeter, stack, scopes, stream):
+    val = stack.pop()
+    tok = interpeter.Token(TYPE="py-obj", VAL={
+        "path": os.path.abspath(val.VAL)
+    })
+    stack.append(tok)
+
+
+def fread(interpeter, stack, scopes, stream):
+    val = stack.pop()
+    with open(val.VAL["path"], "r") as f:
+        tok = interpeter.Token(TYPE="str", VAL=f.read())
+        stack.append(tok)
+
 
 module = {
-    "blah": blah
+    "fopen": fopen,
+    "fread": fread
 }
